6d47e6b312f00a28a985b882a3d1b759925b7343
Yulia <KuznetsovaYP@mpei.ru>
2022-04-04 19:04:16 +0300

    вывод гистограммы svg

--- main.cpp
+++ main.cpp
@@ -59,44 +59,47 @@ make_histogram(const vector<double> &numbers,size_t &bin_count, size_t number_co
 }
 
 void
-show_histogram_text(const vector<size_t> &bins) {
-    size_t max_bin=bins[0];
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
 
-        for(size_t bin: bins)
-        {
-            if(max_bin<bin)
-            {
-                max_bin=bin;
-            }
-        }
+void
+svg_end() {
+    cout << "</svg>\n";
+}
 
-    for(size_t bin:bins)
-        {
-            size_t height=bin;
+void
+svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>"<< text <<"</text>";
+}
 
-            if(max_bin>MAX_ASTERISK)
-            {
-                height=MAX_ASTERISK*(static_cast<double>(bin)/max_bin);
-            }
+void svg_rect(double x, double y, double width, double height,string stroke="darkmagenta", string fill="lightcyan"){
+    cout<< "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
+}
 
-            if (bin<100)
-            {
-                cout<<" ";
-            }
-
-            if (bin<10)
-            {
-                cout<<" ";
-            }
-            cout<<bin<<"|";
-            for(size_t i=0; i<height; i++)
-            {
-                cout<<"*";
-            }
-            cout<<endl;
-
-        }
-        return;
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+for (size_t bin : bins) {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+    top += BIN_HEIGHT;
+}
+    svg_end();
 }
 
 int main()
@@ -128,7 +131,7 @@ int main()
 //Вывод данных
 
 
-        show_histogram_text(bins);
+        show_histogram_svg(bins);
 
 
 
